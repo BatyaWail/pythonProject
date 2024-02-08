5f729baf40168bd8cc00dc29d510f6acc0a2eea5
batya wail <batya4119712@gmail.com>
2024-02-08 02:00:47 +0200

    second

--- FileOperation.py
+++ FileOperation.py
@@ -1,36 +1,74 @@
 import pandas as pd;
 class FileOperation:
+    #1
     def read_csv(self,file_path:str):
         return pd.read_csv(file_path)
+    #2
     def save_to_execl(self,data,file_name:str):
         return pd.DataFrame(data)
 
 class SalesData:
-    def __init__(self):
-        self.df=FileOperation.read_csv("YafeNof.csv")
+    def __init__(self,path:str):
+        #self.df=FileOperation.read_csv(path)
+        x=FileOperation()
+        self.df=x.read_csv(path)
 
+    #3
     def eliminate_duplicates(self):
         # df=FileOperation.read_csv("YafeNof.csv")
         # csvFile.drop_duplicates(inplace=True)
         self.df.drop_duplicates(inplace=True)
+     #4
     def calculate_total_sales(self):
         return self.df['Total']
-    def  calculate_total_sales_per_month(self):
-        self.df.Date=pd.to_datetime(self.df.Date)#להפוך את התאריך לסוג תאריך
+    #6
+
+    def calculate_total_sales_per_month(self):
+        self.df['Date'] = pd.to_datetime(self.df['Date'], format='%d.%m.%Y',errors='coerce')
         # Group by month and calculate the sum of 'Total' for each month
         monthly_sum = self.df.groupby(self.df['Date'].dt.month)['Total'].sum()
-
-        # Display the result
         return monthly_sum
+
+    #7
+    #def identify_best_selling_product(self):
+       # maxProudact = self.df.groupby(self.df.product)["Quantity"]
+       # maxProudact=max(maxProudact.sum())
+       # return maxProudact
     def identify_best_selling_product(self):
-        maxProudact = self.df.groupby(self.df.product)['Quantity'].sum()
-        maxProudact=max(maxProudact)
-        return maxProudact
+        # Group by product and sum the quantities sold
+        product_sales = self.df.groupby('Product')['Quantity'].sum()
+        # Find the product with the maximum total quantity sold
+        best_selling_product = product_sales.idxmax()
+        # Return the name of the best-selling product
+        return best_selling_product
+
+   #7
     def identify_month_with_highest_sales(self):
         maxMonth= self.calculate_total_sales_per_month()
         return  max(maxMonth)
+   #9
     def analys_sales_data(self):
         x=self.identify_best_selling_product()
         y=self.identify_month_with_highest_sales()
-        dictonary=dict(best_selling_product= x, month_with_highest_sales= y )
-        return dictonary
+        dictionary=dict(best_selling_product= x, month_with_highest_sales= y )
+        return dictionary
+    #10
+    def add_to_dict_minimest_seling_and_avg(self):
+        product_sales = self.df.groupby('Product')['Quantity'].sum()
+        # Find the product with the maximum total quantity sold
+        less_selling_product = product_sales.idxmin()
+        # Return the name of the best-selling product
+
+        self.df['Date'] = pd.to_datetime(self.df['Date'], format='%d.%m.%Y',errors='coerce')
+        # Group by month and calculate the sum of 'Total' for each month
+        monthly_sum = self.df.groupby(self.df['Date'].dt.month)['Total'].mean()
+        dictionary=self.analys_sales_data()
+        dictionary['monthly_sum'] = monthly_sum
+        dictionary['less_selling_product'] = less_selling_product
+        return dictionary
+
+    #task 3
+    #11
+   #def calculate_cumulative_sales(self):
+
+
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 # This is a sample Python script.
+from FileOperation import FileOperation
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from FileOperation import SalesData, FileOperation
 
-def print_hi(name):
+
+def print_hi():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # def main():
+    # FileOperation.read_csv("")
+    x = SalesData("YafeNof.csv")
+    print(x.df)
+    x.eliminate_duplicates()
+    print("eliminate_duplicates()",x.df)
+    print("5", x.calculate_total_sales())
+    print("6",x.calculate_total_sales_per_month())
+    print("7",x.identify_best_selling_product())
+    print("8",x.identify_month_with_highest_sales())
+    print("9",x.analys_sales_data())
+    print("10",x.add_to_dict_minimest_seling_and_avg())
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
